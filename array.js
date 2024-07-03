de1229974a858a68e3f4d78e5457e45caa0d486e
gabriel <gabrielmarcaldossantosm@gmail.com>
2024-07-03 14:50:11 -0500

    03/07/24

---
+++ array.js
@@ -0,0 +1,6 @@
+var carros=['porsche ', 'syline ','lamborghinni veneno ', 'mclaren ','ferrari ']
+carros.push=("porsche")
+alert(carros[3])
+alert(carros[4])
+carros.splice([2,2])
+document.write(carros)
